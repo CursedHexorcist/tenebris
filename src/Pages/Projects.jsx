4eaff6897b590ab3d5e194046961d2c7617121bd
CursedHexorcist <cursedopcodes@gmail.com>
2025-05-12 16:30:47 +0700

    Create Projects.jsx

---
+++ src/Pages/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FullWidthTabs from '../components/FullWidthTabs'; // Komponen product yang sudah ada
+
+function Projects() {
+  return (
+    <div className="bg-[#030014] min-h-screen">
+      <FullWidthTabs />
+    </div>
+  );
+}
+
+export default Projects;
